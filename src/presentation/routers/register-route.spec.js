ee8833add759d2b7be89a7824d8384a91bea089d
Douglas Wesley <douglaswesley77@gmail.com>
2024-07-19 19:57:15 -0300

    ensure RegisterRouter return 400 if no password is provided

--- src/presentation/routers/register-route.spec.js
+++ src/presentation/routers/register-route.spec.js
@@ -1,6 +1,6 @@
 class RegisterRouter {
   async exec (httpRequest) {
-    if (!httpRequest.body.username || !httpRequest.body.email) {
+    if (!httpRequest.body.username || !httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -28,6 +28,19 @@ describe('register router', () => {
       body: {
         username: 'any_username',
         password: 'any_password',
+        confirmPassword: 'any_password'
+      }
+    }
+    const httpResponse = await sut.exec(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('should return 400 if password is not provided', async () => {
+    const sut = new RegisterRouter()
+    const httpRequest = {
+      body: {
+        username: 'any_username',
+        email: 'any_email@mail.com',
         confirmPassword: 'any_password'
       }
     }
