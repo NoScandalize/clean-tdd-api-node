ba5ead6d59cccc42fbf4da63f9bac176c19cef62
Douglas Wesley <douglaswesley77@gmail.com>
2024-07-19 19:51:11 -0300

    ensure RegisterRouter return 400 if no email is provided

--- src/presentation/routers/register-route.spec.js
+++ src/presentation/routers/register-route.spec.js
@@ -1,6 +1,6 @@
 class RegisterRouter {
   async exec (httpRequest) {
-    if (!httpRequest.body.username) {
+    if (!httpRequest.body.username || !httpRequest.body.email) {
       return {
         statusCode: 400
       }
@@ -14,6 +14,19 @@ describe('register router', () => {
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
+        password: 'any_password',
+        confirmPassword: 'any_password'
+      }
+    }
+    const httpResponse = await sut.exec(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('should return 400 if email is not provided', async () => {
+    const sut = new RegisterRouter()
+    const httpRequest = {
+      body: {
+        username: 'any_username',
         password: 'any_password',
         confirmPassword: 'any_password'
       }
