142fb489356994f33f82be971001ba9b6a1f7f8e
Douglas Wesley <douglaswesley77@gmail.com>
2024-07-19 20:18:52 -0300

    ensure RegisterRouter returns a custom error if no username, email, password or confirmPassword are provided

--- src/presentation/routers/register-route.spec.js
+++ src/presentation/routers/register-route.spec.js
@@ -1,16 +1,43 @@
 class RegisterRouter {
   async exec (httpRequest) {
     if (!httpRequest || !httpRequest.body) {
-      return {
-        statusCode: 500
-      }
+      return HttpRequest.internalServerError()
     }
     const { username, email, password, confirmPassword } = httpRequest.body
-    if (!username || !email || !password || !confirmPassword) {
-      return {
-        statusCode: 400
-      }
+    if (!username) {
+      return HttpRequest.badRequest('username')
     }
+    if (!email) {
+      return HttpRequest.badRequest('email')
+    }
+    if (!password) {
+      return HttpRequest.badRequest('password')
+    }
+    if (!confirmPassword) {
+      return HttpRequest.badRequest('confirmPassword')
+    }
+  }
+}
+
+class HttpRequest {
+  static badRequest (paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName)
+    }
+  }
+
+  static internalServerError () {
+    return {
+      statusCode: 500
+    }
+  }
+}
+
+class MissingParamError extends Error {
+  constructor (paramName) {
+    super(`Missing param: ${paramName}`)
+    this.name = 'MissingParamError'
   }
 }
 
@@ -26,6 +53,7 @@ describe('register router', () => {
     }
     const httpResponse = await sut.exec(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('username'))
   })
 
   test('should return 400 if email is not provided', async () => {
@@ -39,6 +67,7 @@ describe('register router', () => {
     }
     const httpResponse = await sut.exec(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
   test('should return 400 if password is not provided', async () => {
@@ -52,6 +81,7 @@ describe('register router', () => {
     }
     const httpResponse = await sut.exec(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 
   test('should return 400 if confirmPassword is not provided', async () => {
@@ -65,6 +95,7 @@ describe('register router', () => {
     }
     const httpResponse = await sut.exec(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('confirmPassword'))
   })
 
   test('should return 500 if no httpRequest is provided', async () => {
