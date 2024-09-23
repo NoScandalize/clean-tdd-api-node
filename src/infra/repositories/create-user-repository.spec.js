f9ced0d2aad55a291f8a6e58ba17efd29619f7ab
Douglas Wesley <douglaswesley77@gmail.com>
2024-09-23 16:34:52 -0300

    ensure CreateUserRepository throw if no userModel is provided

--- src/infra/repositories/create-user-repository.spec.js
+++ src/infra/repositories/create-user-repository.spec.js
@@ -1,6 +1,10 @@
 const MissingParamError = require('../../utils/errors/missing-param-error')
 
 class CreateUseRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
   async create (username, email, hashedPassword) {
     if (!username) {
       throw new MissingParamError('username')
@@ -11,10 +15,18 @@ class CreateUseRepository {
     if (!hashedPassword) {
       throw new MissingParamError('hashedPassword')
     }
+    const user = await this.userModel.insertOne({ username, email, password: hashedPassword })
+    return user
   }
 }
 
 describe('createUser repository', () => {
+  test('should throw if no userModel is provided', async () => {
+    const sut = new CreateUseRepository()
+    const promise = sut.create('any_username', 'valid_email@mail.com', 'any_hashedPassord')
+    expect(promise).rejects.toThrow()
+  })
+
   test('should throw if no params are provided', async () => {
     const sut = new CreateUseRepository()
     expect(sut.create()).rejects.toThrow(new MissingParamError('username'))
