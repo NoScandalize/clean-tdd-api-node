3d7ad46e837aa8aaef2fcbdbbc7d159952c7e4ee
Douglas Wesley <douglaswesley77@gmail.com>
2024-09-23 11:00:27 -0300

    move test cors to its own file

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -1,22 +1,12 @@
 const request = require('supertest')
 const app = require('./app')
 
-describe('', () => {
+describe('app setup', () => {
   test('should disable x-powered-by header', async () => {
     app.get('/test_x_powered_by', (req, res) => {
       res.send('')
     })
     const res = await request(app).get('/')
     expect(res.headers['x-powered-by']).toBeUndefined()
-  })
-
-  test('should enable CORS', async () => {
-    app.get('/test_cors', (req, res) => {
-      res.send('')
-    })
-    const res = await request(app).get('/')
-    expect(res.headers['access-control-allow-origin']).toBe('*')
-    expect(res.headers['access-control-allow-methods']).toBe('*')
-    expect(res.headers['access-control-allow-headers']).toBe('*')
   })
 })
