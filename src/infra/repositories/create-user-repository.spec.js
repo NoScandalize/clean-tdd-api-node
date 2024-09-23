9dda45afe3ce5ececc5ee815206216bb2985a26f
Douglas Wesley <douglaswesley77@gmail.com>
2024-09-23 16:42:25 -0300

    move sut creation to a factory helper method

--- src/infra/repositories/create-user-repository.spec.js
+++ src/infra/repositories/create-user-repository.spec.js
@@ -1,5 +1,13 @@
 const MissingParamError = require('../../utils/errors/missing-param-error')
+const MongoHelper = require('../../infra/helpers/mongo-helper')
 
+let db
+
+const makeSut = () => {
+  const userModel = db.collection('users')
+  const sut = new CreateUseRepository(userModel)
+  return { sut, userModel }
+}
 class CreateUseRepository {
   constructor (userModel) {
     this.userModel = userModel
@@ -21,6 +29,19 @@ class CreateUseRepository {
 }
 
 describe('createUser repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+    db = await MongoHelper.db
+  })
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
   test('should throw if no userModel is provided', async () => {
     const sut = new CreateUseRepository()
     const promise = sut.create('any_username', 'valid_email@mail.com', 'any_hashedPassord')
@@ -28,7 +49,7 @@ describe('createUser repository', () => {
   })
 
   test('should throw if no params are provided', async () => {
-    const sut = new CreateUseRepository()
+    const { sut } = makeSut()
     expect(sut.create()).rejects.toThrow(new MissingParamError('username'))
     expect(sut.create('any_username')).rejects.toThrow(new MissingParamError('email'))
     expect(sut.create('any_username', 'valid_email')).rejects.toThrow(new MissingParamError('hashedPassword'))
